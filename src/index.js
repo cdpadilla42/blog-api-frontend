13381061d1d42450284635d63241c468c83d7352
Chris Padilla <cdpadilla42@gmail.com>
2020-04-24 12:50:40 -0500

    Write JS functions for connecting Content to DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // A reminder to make your JS Modular here. You will need seperate functions for seperate pages, I would imagine. This is your first multipage effort, afterall. Good Luck!
 const body = document.querySelector('.content');
 
+// Data Retrieval
+
 async function getContent() {
   const response = await fetch('http://localhost:3000/api/posts', {
     mode: 'cors',
@@ -9,14 +11,34 @@ async function getContent() {
   return content.postList;
 }
 
+// Connect DOM to Content
+
 async function displayContent() {
   const content = await getContent();
   console.log(content);
   content.forEach((post) => {
-    let postDiv = document.createElement('div');
-    postDiv.innerText = post.title;
+    let postDiv = createPostElement(post);
     body.appendChild(postDiv);
   });
+}
+
+// DOM Manipulation
+
+function createPostElement(postContent) {
+  let postDiv = document.createElement('div');
+  let titleDiv = document.createElement('div');
+  let textDiv = document.createElement('div');
+  let dateDiv = document.createElement('div');
+
+  titleDiv.innerText = postContent.title;
+  textDiv.innerText = postContent.text;
+  dateDiv.innerText = postContent.date;
+
+  postDiv.appendChild(titleDiv);
+  postDiv.appendChild(textDiv);
+  postDiv.appendChild(dateDiv);
+
+  return postDiv;
 }
 
 displayContent();
